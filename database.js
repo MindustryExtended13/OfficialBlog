6660b69b00b9d5f3d9f2405452205b24149a9561
TheEE145 <Arhitector2009@gmail.com>
2024-03-05 22:26:10 +0200

    New post

--- database.js
+++ database.js
@@ -11,5 +11,6 @@ function makepage(name, author, date, icon, link, description) {
 
 const pages = [
     //makepage("Test", "TheEE145", "2/22/2024", "graphics/icon.png", "pages/test.html", "Pootis"),
-    makepage("Space Exploration (Mindustry Edition)", "TheEE145", "2/22/2024", "graphics/pages/se.png", "pages/se.html", "The space exploration mod will be rework mindustry mechanics in total and adds lots of content")
+    makepage("SE inventory base system", "TheEE145", "03/05/2024", "graphics/pages/box.png", "pages/se-inventory.html", "The SE inventory system will rework cargo and bulding, even basics"),
+    makepage("Space Exploration (Mindustry Edition)", "TheEE145", "02/22/2024", "graphics/pages/se.png", "pages/se.html", "The space exploration mod will be rework mindustry mechanics in total and adds lots of content")
 ]
